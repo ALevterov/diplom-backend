1065a045c8a30d45494df63021777b924693a528
AlexanderLevterov <alevterov@inbox.ru>
2023-02-24 20:57:56 +0300

    changes

--- controllers/thematicController.js
+++ controllers/thematicController.js
@@ -1,20 +1,29 @@
-const { defineThematic } = require("../parsing/defineWebsiteThematic");
-const { getSquidLogs } = require("../parsing/getSquidLogs");
-const { transformSquidLogs } = require("../parsing/transformSquidLogs");
+const { defineThematic } = require('../parsing/defineWebsiteThematic')
+const { getSquidLogs } = require('../parsing/getSquidLogs')
+const { transformSquidLogs } = require('../parsing/transformSquidLogs')
 
 class ThematicController {
-	async defineTheme(req, res, next) {
-		console.log('define theme!')
-		try {
-			const thematics = await defineThematic('https://skillbox.ru')
-			// const transformedLogs = transformSquidLogs(getSquidLogs())
-			// console.log(transformedLogs)
-			return res.json({response: thematics})
-		} catch (e) {
-			console.log(e)
-			res.json({response: e.message})
-		}
-	}
+  async defineTheme(req, res, next) {
+    console.log('define theme!')
+    try {
+      let url = 'https://skillbox.ru/asdasdas/'
+      let match = url.match(/https:\/\/[a-z|а-я|.|\d+]+/i)
+      if (match) {
+        url = match[0]
+        console.log(url)
+      } else {
+        console.log('invalid url string')
+        return
+      }
+      const thematics = await defineThematic(url)
+      // const transformedLogs = transformSquidLogs(getSquidLogs())
+      // console.log(transformedLogs)
+      return res.json({ response: thematics })
+    } catch (e) {
+      console.log(e)
+      res.json({ response: e.message })
+    }
+  }
 }
 
 module.exports = new ThematicController()
--- parsing/defineWebsiteThematic.js
+++ parsing/defineWebsiteThematic.js
@@ -1,58 +1,61 @@
 const axios = require('axios')
-const jsdom = require("jsdom"); 
-const HttpsProxyAgent = require('https-proxy-agent');
-const { Thematic } = require('../models');
-const { getLinks } = require('./getLinks');
-const { defineWordCount } = require('./defineWordCount');
-const { cutStylesAndScripts } = require('./cutStylesAndScripts');
-const { defineThematicByWordsCount } = require('./defineThematicByWordsCount');
+const jsdom = require('jsdom')
+const HttpsProxyAgent = require('https-proxy-agent')
+const { Thematic } = require('../models')
+const { getLinks } = require('./getLinks')
+const { defineWordCount } = require('./defineWordCount')
+const { cutStylesAndScripts } = require('./cutStylesAndScripts')
+const { defineThematicByWordsCount } = require('./defineThematicByWordsCount')
 
-let httpsAgent = new HttpsProxyAgent({protocol: 'http', host: '10.33.78.12', port: 3333, rejectUnauthorized: false});
+// let httpsAgent = new HttpsProxyAgent({
+//   protocol: 'http',
+//   host: '10.33.78.12',
+//   port: 3333,
+//   rejectUnauthorized: false,
+// })
 
-instance = axios.create({httpsAgent, proxy: false})
+// instance = axios.create({httpsAgent, proxy: false})
+instance = axios.create()
 
-const { JSDOM } = jsdom;
-const defineThematic = async (url) => { 
-	try {
-		let thematics = await Thematic.findAll()
+const { JSDOM } = jsdom
+const defineThematic = async url => {
+  try {
+    let thematics = await Thematic.findAll()
 
-		thematics= thematics.map(thm => {
-			return thm.dataValues
-		})
+    thematics = thematics.map(thm => {
+      return thm.dataValues
+    })
 
-		const {data} = await instance.get(url)
-		const dom = new JSDOM(data)
-		
-		cutStylesAndScripts(dom)
+    const { data } = await instance.get(url)
+    const dom = new JSDOM(data)
 
-		const text =  dom.window.document.body.innerHTML
-		const links = getLinks(dom) // получаем ссылки первого уровня
-		defineWordCount(text, thematics) // определяем количество слов по темам на основной странице
-		console.log(url+' completed!!')
-			for (const link of links) {
-				try {
-					const {data} = await instance.get(url + link)
-					const dom = new JSDOM(data)
-					cutStylesAndScripts(dom) // обрезаем скрипты и стили
-					const text =  dom.window.document.body.innerHTML
-	
-					defineWordCount(text, thematics) // определяем количество слов по темам по ссылке первого уровня
-					console.log(url + link + ' completed!!')
-				} catch(e) {
-					console.log(e)
-				}
-			}
+    cutStylesAndScripts(dom)
 
-			
-		const definedThematic = defineThematicByWordsCount(thematics)
-		console.log('theme defined!!!')
-		return {thematics, definedThematic}
+    const text = dom.window.document.body.innerHTML
+    const links = getLinks(dom) // получаем ссылки первого уровня
+    defineWordCount(text, thematics) // определяем количество слов по темам на основной странице
+    console.log(url + ' completed!!')
+    for (const link of links) {
+      try {
+        const { data } = await instance.get(url + link)
+        const dom = new JSDOM(data)
+        cutStylesAndScripts(dom) // обрезаем скрипты и стили
+        const text = dom.window.document.body.innerHTML
 
-	} catch(e) {
-		console.log(e)
-		return e
-	}
+        defineWordCount(text, thematics) // определяем количество слов по темам по ссылке первого уровня
+        console.log(url + link + ' completed!!')
+      } catch (e) {
+        console.log(e)
+      }
+    }
 
+    const definedThematic = defineThematicByWordsCount(thematics)
+    console.log('theme defined!!!')
+    return { thematics, definedThematic }
+  } catch (e) {
+    console.log(e)
+    return e
+  }
 }
 
-module.exports = {defineThematic}
+module.exports = { defineThematic }
